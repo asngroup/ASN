dbd95bca2c7c15a0ba7f54f7a9f392431a0e3420
RashedulHaqueRasel1 <rashedulhaquerasel1@gmail.com>
2024-07-07 21:19:54 +0600

    Create Branch

--- src/Common/Footer.jsx
+++ src/Common/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
 
             <footer className="footer footer-center bg-base-300 text-base-content p-4">
                 <aside>
-                    <p>Copyright © ${new Date().getFullYear()} - All right reserved by ASN Industries Ltd</p>
+                    <p>Copyright © ${new Date().getFullYear()} - All right reserved by ASN Industries Ltd.</p>
                 </aside>
             </footer>
             
